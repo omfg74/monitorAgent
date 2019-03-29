8f4ecc0c55587f99e72cbf9e6a2c16fa5d4bcc5d
Alexander <podsh74@gmail.com>
2019-03-29 22:11:52 +0500

    2123

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) {
+while (true){
+        Thread exec = new ExecThread();
+        exec.run();
+    }
+    }
+}
